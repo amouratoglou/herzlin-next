22ab9fa8e07ce24744efb119e84be5c287be93eb
amouratoglou <amouratoglou@amouratglousMBP.fibertel.com.ar>
2021-02-09 00:10:00 -0300

    updated logo css

--- frontend/components/sections/testimonials-group.js
+++ frontend/components/sections/testimonials-group.js
@@ -35,7 +35,7 @@ const TestimonialsGroup = ({ data }) => {
           <div>
             <Image
               media={selectedTestimonial.logo}
-              className="h-8 w-auto mb-6 sm:mb-10 mt-2 sm:mt-0"
+              className="h-auto w-5/12	mb-6 sm:mb-10 mt-2 sm:mt-0"
             />
             <p className="italic mb-6">"{selectedTestimonial.text}"</p>
             <p className="font-bold text-base sm:text-sm">
