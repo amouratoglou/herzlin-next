9d644fb958f6b864c470c02f6378f4f46751635d
amouratoglou <mouratoglou@gmail.com>
2021-02-12 11:01:47 -0300

    reverted previous vercel deploy

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,5 +1,5 @@
 export function getStrapiURL(path) {
-  return `${process.env.NEXT_PUBLIC_STRAPI_API_URL || 'https://herzlin.herokuapp.com'}${path}`;
+  return `${process.env.NEXT_PUBLIC_STRAPI_API_URL || 'http://localhost:1337'}${path}`;
 }
 
 // Helper to make GET requests to Strapi
